5a5e0d0a50aced42b6e29da8e33bf4b81d5a9031
ikimaka <ikimaka@yandex.ru>
2024-08-20 09:55:36 +0300

    Decompose. Interface Child

--- app/src/main/java/com/ikimaka/mvidecompose/ui/content/RootContent.kt
+++ app/src/main/java/com/ikimaka/mvidecompose/ui/content/RootContent.kt
@@ -23,14 +23,15 @@ fun RootContent(
                 stack = component.stack
             ) {
                 when (val instance = it.instance) {
-                    is ContactListComponent -> {
-                        Contacts(component = instance)
+
+                    is DefaultRootComponent.Child.AddContact -> {
+                        AddContact(component = instance.component)
                     }
-                    is AddContactComponent -> {
-                        AddContact(component = instance)
+                    is DefaultRootComponent.Child.ContactList -> {
+                        Contacts(component = instance.component)
                     }
-                    is EditContactComponent -> {
-                        EditContact(component = instance)
+                    is DefaultRootComponent.Child.EditContact -> {
+                        EditContact(component = instance.component)
                     }
                 }
             }
